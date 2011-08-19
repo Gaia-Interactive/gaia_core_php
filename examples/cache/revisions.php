d3e0d259d52ae588c990a0d19298d7fee6f622c9
John Loehrer <jloehrer@gaiaonline.com>
2011-08-19 14:52:05 +0000

    adding docs for revision example

--- examples/cache/revisions.php
+++ examples/cache/revisions.php
@@ -8,22 +8,36 @@ use Gaia\Test\Tap;
 include __DIR__ . '/../common.php';
 include __DIR__ . '/connection.php';
 
-
-class UserInfo {
+/**
+* When writing caching solutions, the preferred approach is to have a single 'domain' model (see 
+* http://en.wikipedia.org/wiki/Domain_model ) to control the business logic and caching for everything
+* related to that relevant domain. But sometimes, the real world is messier than that. Sometimes it
+* is best to cache large chunks of aggregated information. For example, you might want to cache a
+* user's entire profile page. Profile pages are difficult to cache because they usually contain lots
+* of information from many different tables. This means they are also expensive to render. If I cache 
+* the entire profile page, I skip instantiating all those different objects and making all those queries.
+* I can get it all from one cache key.
+*
+* But what if information changes? This is where revisions come in handy. The cache revision is a layer 
+* of indirection in the cache key. Normally when constructing a cache key for something, I might do 
+* something like:
+*    /user/profile/{$user_id}
+* But I can bust the cache, if I also append a revision to that key. Every time I change something related 
+* to that page, I make sure to also increment the revision. 
+*    /user/profile/{$user_id}/rev/{$rev}
+* I can use another cache key to store that revision. This means each time I want to load the profile
+* page cache, I must first get the revision that is associated with it.
+*
+* There are some tricks to make this work though. If all I do is increment a cache key for the revision 
+* number, what happens if the revision number gets evicted from the cache? Instead of using a simple 
+* incrementing id, I can instead create a revision out of the current time, along with another integer 
+* that is unlikely to collide with other numbers in our server farm. Now, even if the revision gets 
+* evicted from the cache, I wont accidentally repeat an old revision number and get a stale version of 
+* a cached page.
+*/
+class UserRev {
     
-    static protected $names = array();
-    
-    public static function getName( $id ){
-        return self::$names[ $id ];
-    }
-    
-    public static function setName( $id, $name ){
-        self::$names[ $id ] = $name;
-        self::rev( $id, TRUE );
-        return $name;
-    }
-    
-    public static function rev( $id, $refresh = FALSE ){
+    public static function get( $id, $refresh = FALSE ){
         $cacher = self::cache();
         $key = '/rev/' . $id;
         $res = ( ! $refresh ) ? $cacher->get($key) : '';
@@ -39,21 +53,42 @@ class UserInfo {
 
 }
 
+class UserName {
+    
+    static protected $names = array();
+    
+    public static function get( $id ){
+        return self::$names[ $id ];
+    }
+    
+    public static function set( $id, $name ){
+        self::$names[ $id ] = $name;
+        UserRev::get( $id, TRUE );
+        return $name;
+    }
+}
 
-class UserPage {
+
+class UserMessage {
     
     protected $user_id;
+    protected $message;
     const CACHE_TIMEOUT = 30;
     
-    function __construct( $user_id ){
+    function __construct( $user_id, $message ){
         $this->user_id = $user_id;
+        $this->message = $message;
     }
     
     function render(){
-        $key = $this->user_id .'/' . UserInfo::rev( $this->user_id );
+        $key = $this->user_id .'/' . UserRev::get( $this->user_id );
         $result = self::cache()->get( $key );
         if( $result ) return $result;
-        $result = sprintf("<h1>hello, %s, today at %s (%s)<h1>", UserInfo::getName( $this->user_id ), date('Y/m/d H:i:s'), microtime(TRUE));
+        $result = sprintf("<h1>%s, %s, today at %s (%s)<h1>", 
+            $this->message,
+            UserName::get( $this->user_id ), 
+            date('Y/m/d H:i:s'), 
+            microtime(TRUE));
         self::cache()->set( $key, $result, MEMCACHE_COMPRESSED, self::CACHE_TIMEOUT );
         return $result;
     }
@@ -66,19 +101,29 @@ class UserPage {
 // ----
 // DEMO 
 
-Tap::plan(3);
+Tap::plan(7);
 $user_id = 1;
-UserInfo::setName($user_id, $name = 'billy' );
-$page = new UserPage( $user_id );
-$before = $page->render();
-UserInfo::setName( $user_id, $name = 'bob' );
-$after = $page->render();
-$again = $page->render();
-Tap::like($before, '/billy/', 'before, page has the name billy in it');
-Tap::like($after, '/bob/', 'after, page has the name bob in it');
-Tap::is( $after, $again, 'when I load the page again, get the same content');
+Username::set($user_id, $name = 'billy' );
+$hello = new UserMessage( $user_id, 'hello');
+$goodbye = new UserMessage( $user_id, 'hello');
+
+
+
+Tap::like( $before = $hello->render(), '/billy/', 'before, hello message has the name billy in it');
+UserName::set( $user_id, $name = 'bob' );
+Tap::like($after = $hello->render(), '/bob/', 'after name change, hello message has the name bob in it');
+Tap::is( $after, $hello->render(), 'when I load the message again, I get cached content');
 
 Tap::debug( $before, 'before name change');
 Tap::debug( $after, 'after name change');
+
+Tap::like( $before = $goodbye->render(), '/bob/', 'before, goodbye message has the name bob in it');
+UserName::set( $user_id, $name = 'bubba' );
+Tap::like($after = $goodbye->render(), '/bubba/', 'after name change, goodbye message has the name bubba in it');
+Tap::is( $after, $goodbye->render(), 'when I load the message again, I get cached content');
+
+UserRev::get( $user_id, TRUE );
+Tap::isnt( $after, $goodbye->render(), 'when I load the message after cache busting, I get new content');
+
 
 
