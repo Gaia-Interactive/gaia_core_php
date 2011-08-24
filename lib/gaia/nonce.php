cf332fc6443ad2e4d32c74380fb427eafaa5afc0
John Loehrer <jloehrer@gaiaonline.com>
2011-08-24 11:57:15 +0000

    creating a more secure nonce

--- lib/gaia/nonce.php
+++ lib/gaia/nonce.php
@@ -10,23 +10,50 @@ namespace Gaia;
 class Nonce {
 
     const INTERVAL = 3600;
-    protected $digest_length = 40;
+    protected static $charset;
+    protected $chunk_length = 40;
     protected $secret = '';
     
-    public function __construct( $secret, $digest_length = 40 ){
+    public function __construct( $secret, $chunk_length = 40 ){
         $this->secret = $secret;
-        $this->digest_length = $digest_length;
+        $this->chunk_length = $chunk_length;
     }
     
     public function create($token, $expires = null) {
         if ($expires === NULL) $expires = time() + self::INTERVAL;
-        return substr( sha1($token . "-" . $expires . "-" . $this->secret), 0, $this->digest_length) . $expires;
+        $rand = self::rand($this->chunk_length);
+        $nonce =  substr(sha1( $rand . $expires ), 0, $this->chunk_length);
+        $digest = substr(sha1( $rand . $token . $this->secret . $expires ), 0, $this->chunk_length);
+        return $nonce . $digest . $rand . $expires;        
     }
 
-    public function check($input, $token ) {
-        if( ! is_string( $input ) ) return FALSE;
-        if( ! preg_match('/^[a-f0-9]{' . $this->digest_length . '}([0-9]+)$/', $input, $matches ) ) return FALSE;
-        if ($input != Nonce::create($token, $expires = $matches[1])) return FALSE;
-        return $expires >= time() ? TRUE : FALSE;
+    public function check($hash, $token ) {
+        if( ! is_string( $hash ) ) return FALSE;
+        if( strlen( $hash ) < ($this->chunk_length * 3) + 1 ) return FALSE;
+        $nonce = substr($hash, 0, $this->chunk_length);
+        $digest = substr($hash, $this->chunk_length, $this->chunk_length);
+        $rand = substr($hash, $this->chunk_length * 2, $this->chunk_length);
+        $expires = substr($hash, $this->chunk_length * 3);
+        if( ! is_numeric( $expires ) ) return false;
+        if( $expires < time() ) return FALSE;
+        if( substr( sha1( $rand . $expires ), 0, $this->chunk_length) != $nonce ) return FALSE;
+        if( substr( sha1( $rand . $token . $this->secret . $expires ), 0, $this->chunk_length) != $digest ) return FALSE;
+        return TRUE;
+    }
+    
+    protected static function rand($length = 10 ){
+        $rand = '';
+        $charset = self::charset();
+        $charset_len = strlen( $charset ) - 1;
+        for ($i=0; $i<$length; $i++) $rand .= $charset[(mt_rand(0,$charset_len))];
+        return $rand;
+    }
+    
+    protected static function charset(){
+        if( isset( self::$charset ) ) return self::$charset;
+        return self::$charset =  
+                    implode('', range('a', 'z')) . 
+                    implode('', range('a', 'z')) .
+                    implode('', range('0', '9')) ;
     }
 }
