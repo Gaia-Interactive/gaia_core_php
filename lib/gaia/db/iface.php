83e5d858693b715888c11c6c64f49aaa78ceac10
John Loehrer <jloehrer@gaiaonline.com>
2011-09-07 22:08:06 -0700

    adding a simple example of using codeigniter db layer with gaia

--- lib/gaia/db/iface.php
+++ lib/gaia/db/iface.php
@@ -5,4 +5,7 @@ interface IFace {
     public function begin();
     public function rollback();
     public function commit();
+    public function execute($query);
+    public function format_query($query);
+    public function format_query_args( $query, array $args );
 }
