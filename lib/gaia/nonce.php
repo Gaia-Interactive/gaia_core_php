3c1b47ceecda85ba4ba4cb3d8b87e8e81d926768
John Loehrer <jloehrer@gaiaonline.com>
2011-10-20 00:21:58 -0700

    simplifying the nonce class. tightening the logic, shortening the hash string from 131 characters down to 91 max. min is 13 tho this hash is almost useless

--- lib/gaia/nonce.php
+++ lib/gaia/nonce.php
@@ -8,52 +8,77 @@
 namespace Gaia;
 
 class Nonce {
-
+    // default number of seconds a nonce is good for.
     const INTERVAL = 3600;
-    protected static $charset;
+    
+    // seed characters for random string.
+    protected static $charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    
+    // how many characters to use in the digest and random strings.
     protected $chunk_length = 40;
+    
+    // the private key of the nonce, known only to the application. Keep this value safe!
     protected $secret = '';
     
-    public function __construct( $secret, $chunk_length = 40 ){
+    /**
+    * class constructor.
+    * pass in the secret seed value for the nonce.
+    * if the secret is leaked, the nonce is fairly useless.
+    * The more random you make the secret, the better.
+    * Can't specify exactly how long to make the nonce since you need 11 chars to specify a 
+    * timestamp and at least 1 character for the random chunk and 1 character for the checksum.
+    * in practice a number between 30 and 91 is pretty good. 91 is virtually unhackable.
+    */
+    public function __construct( $secret, $length = 91 ){
         $this->secret = $secret;
-        $this->chunk_length = $chunk_length;
+        $this->chunk_length = floor( ($length - 11) / 2 );
+        if( $this->chunk_length < 1 ) $this->chunk_length = 1;
     }
     
+    /**
+    * create a nonce hash based on a token. A token is a publicly known variable that is consistent
+    * from one request to the next. Examples of a good token are:
+    *     user id
+    *     session id
+    *     ip address
+    *     form input variable that doesn't change.
+    *     ssh public key.
+    *
+    * The token passed here must be the same as the value passed into check.
+    * in other words:  $nonce->check( $nonce->create( $token ), $token ) == TRUE;
+    * The optional expires parameter specifies a unix timestamp on how long the nonce is good for.
+    * give yourself some wiggle room. An hr or two.
+    */
     public function create($token, $expires = null) {
         if ($expires === NULL) $expires = time() + self::INTERVAL;
         $rand = self::rand($this->chunk_length);
-        $nonce =  substr(sha1( $rand . $expires ), 0, $this->chunk_length);
         $digest = substr(sha1( $rand . $token . $this->secret . $expires ), 0, $this->chunk_length);
-        return $nonce . $digest . $rand . $expires;        
+        return $digest . $rand . str_pad( $expires, 11, '0', STR_PAD_LEFT);        
     }
-
+    
+    /**
+    * given the hash value returned from the create method above, is the nonce valid for a given token?
+    * returns a boolean, TRUE if the hash validates and isn't expired.
+    */
     public function check($hash, $token ) {
         if( ! is_string( $hash ) ) return FALSE;
-        if( strlen( $hash ) < ($this->chunk_length * 3) + 1 ) return FALSE;
-        $nonce = substr($hash, 0, $this->chunk_length);
-        $digest = substr($hash, $this->chunk_length, $this->chunk_length);
-        $rand = substr($hash, $this->chunk_length * 2, $this->chunk_length);
-        $expires = substr($hash, $this->chunk_length * 3);
-        if( ! is_numeric( $expires ) ) return false;
+        if( strlen( $hash ) < ($this->chunk_length * 2) + 1 ) return FALSE;
+        $digest = substr($hash, 0, $this->chunk_length);
+        $rand = substr($hash, $this->chunk_length, $this->chunk_length);
+        $expires = ltrim(substr($hash, $this->chunk_length * 2), '0');
+        if( ! is_numeric( $expires ) ) return FALSE;
         if( $expires < time() ) return FALSE;
-        if( substr( sha1( $rand . $expires ), 0, $this->chunk_length) != $nonce ) return FALSE;
         if( substr( sha1( $rand . $token . $this->secret . $expires ), 0, $this->chunk_length) != $digest ) return FALSE;
         return TRUE;
     }
     
+   /**
+    * create a random string of N characters from the charset
+    */
     protected static function rand($length = 10 ){
         $rand = '';
-        $charset = self::charset();
-        $charset_len = strlen( $charset ) - 1;
-        for ($i=0; $i<$length; $i++) $rand .= $charset[(mt_rand(0,$charset_len))];
+        $charset_len = strlen( self::$charset ) - 1;
+        for ($i=0; $i<$length; $i++) $rand .= self::$charset[(mt_rand(0,$charset_len))];
         return $rand;
-    }
-    
-    protected static function charset(){
-        if( isset( self::$charset ) ) return self::$charset;
-        return self::$charset =  
-                    implode('', range('a', 'z')) . 
-                    implode('', range('a', 'z')) .
-                    implode('', range('0', '9')) ;
     }
 }
--- tests/nonce.t
+++ tests/nonce.t
@@ -3,7 +3,7 @@
 include_once __DIR__ . '/common.php';
 use Gaia\Test\Tap;
 use Gaia\Nonce;
-Tap::plan(3);
+Tap::plan(4);
 $token = 'demo' . time();
 $secret = 'abc123demo' . microtime(TRUE);
 $n = new Nonce($secret);
@@ -11,8 +11,14 @@ Tap::ok( $n->check( $nonce = $n->create( $token, $expires = time() + 1 ), $token
 Tap::debug($nonce);
 Tap::ok( ! $n->check( $nonce = $n->create($token, time() - 1), $token), 'old nonce is expired');
 Tap::debug( $nonce );
-$n = new Nonce($secret, 10 );
+$n = new Nonce($secret, 39 );
 $expires = time() + 10;
 $nonce = $n->create( $token, $expires );
-Tap::is( strlen( $nonce), 10 * 3 + strlen( $expires), 'nonce chunk length overriden to create a shorter nonce (less secure)');
+Tap::is( strlen( $nonce), 39, 'nonce chunk length overriden to create a shorter nonce (less secure)');
+Tap::debug( $nonce );
+
+$n = new Nonce($secret, 13 );
+$expires = time() + 10;
+$nonce = $n->create( $token, $expires );
+Tap::is( strlen( $nonce), 13, 'nonce chunk length overriden to create shortest nonce possible: about 1000 variants per sec ...');
 Tap::debug( $nonce );
