40c77e0afa275564d7132ccbb6204b91dbda6b4c
John Loehrer <jloehrer@gaiaonline.com>
2011-11-02 07:47:10 -0700

    fixing pdo lib detection in stockpile tests

--- tests/stockpile/litepdo/setup.php
+++ tests/stockpile/litepdo/setup.php
@@ -7,6 +7,10 @@ if( ! class_exists('\PDO') ){
     Tap::plan('skip_all', 'php-pdo not installed');
 }
 
+if( ! in_array( 'sqlite', PDO::getAvailableDrivers()) ){
+    Tap::plan('skip_all', 'this version of PDO does not support sqlite');
+}
+
 DB\Connection::load(array('test'=>function () {
         return new DB\Driver\PDO( 'sqlite:/tmp/stockpile.db');
     }
--- tests/stockpile/mypdo/setup.php
+++ tests/stockpile/mypdo/setup.php
@@ -2,6 +2,14 @@
 require __DIR__ . '/../lib/setup.php';
 use Gaia\Test\Tap;
 
+if( ! class_exists('\PDO') ){
+    Tap::plan('skip_all', 'php-pdo not installed');
+}
+
+if( ! in_array( 'mysql', PDO::getAvailableDrivers()) ){
+    Tap::plan('skip_all', 'this version of PDO does not support mysql');
+}
+
 if( ! @fsockopen('127.0.0.1', '3306')) {
     Tap::plan('skip_all', 'mysql-server not running on localhost');
 }
