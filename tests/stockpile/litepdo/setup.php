14155035918a57168d4756cafcd1226fd87d371b
John Loehrer <jloehrer@gaiaonline.com>
2011-11-02 07:38:07 -0700

    simplifying db instantiation in stockpile tests

--- tests/stockpile/litepdo/setup.php
+++ tests/stockpile/litepdo/setup.php
@@ -8,9 +8,6 @@ if( ! class_exists('\PDO') ){
 }
 
 DB\Connection::load(array('test'=>function () {
-        $db = new DB\Driver\PDO( 'sqlite:/tmp/stockpile.db');
-        $db->setAttribute(\PDO::ATTR_STATEMENT_CLASS, array('Gaia\DB\Driver\PDOStatement', array($db)));
-        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT );
-        return $db;
+        return new DB\Driver\PDO( 'sqlite:/tmp/stockpile.db');
     }
 ));
--- tests/stockpile/mypdo/setup.php
+++ tests/stockpile/mypdo/setup.php
@@ -7,9 +7,6 @@ if( ! @fsockopen('127.0.0.1', '3306')) {
 }
 
 Gaia\DB\Connection::load( array('test'=>function () {
-    $db = new Gaia\DB\Driver\PDO('mysql:host=127.0.0.1;dbname=test;port=3306');
-    $db->setAttribute(\PDO::ATTR_STATEMENT_CLASS, array('Gaia\DB\Driver\PDOStatement', array($db)));
-    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT );
-    return $db;
+    return new Gaia\DB\Driver\PDO('mysql:host=127.0.0.1;dbname=test;port=3306');
 }
 ) );
