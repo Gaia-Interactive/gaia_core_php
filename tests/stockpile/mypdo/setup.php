e91d4cc2709b0e3e1176c2b6aca66391284dcfed
John Loehrer <jloehrer@gaiaonline.com>
2011-11-09 06:18:42 -0800

    standardizing test preconditions

--- tests/stockpile/mypdo/setup.php
+++ tests/stockpile/mypdo/setup.php
@@ -1,18 +1,10 @@
 <?php
-require __DIR__ . '/../lib/setup.php';
 use Gaia\Test\Tap;
 
-if( ! class_exists('\PDO') ){
-    Tap::plan('skip_all', 'php-pdo not installed');
-}
-
-if( ! in_array( 'mysql', PDO::getAvailableDrivers()) ){
-    Tap::plan('skip_all', 'this version of PDO does not support mysql');
-}
-
-if( ! @fsockopen('127.0.0.1', '3306')) {
-    Tap::plan('skip_all', 'mysql-server not running on localhost');
-}
+include __DIR__ . '/../lib/setup.php';
+include __DIR__ . '/../../assert/pdo_installed.php';
+include __DIR__ . '/../../assert/pdo_mysql_installed.php';
+include __DIR__ . '/../../assert/mysql_running.php';
 
 Gaia\DB\Connection::load( array('test'=>function () {
     return new Gaia\DB\Driver\PDO('mysql:host=127.0.0.1;dbname=test;port=3306');
