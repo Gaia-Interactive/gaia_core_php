4d580c38530fb14f521b35a2a9d55f811dfbcbb5
John Loehrer <jloehrer@gaiaonline.com>
2011-11-10 15:52:54 -0800

    more stockpile tests

---
+++ tests/stockpile/mypdo-gaia/setup.php
@@ -0,0 +1,12 @@
+<?php
+use Gaia\Test\Tap;
+
+include __DIR__ . '/../lib/setup.php';
+include __DIR__ . '/../../assert/pdo_installed.php';
+include __DIR__ . '/../../assert/pdo_mysql_installed.php';
+include __DIR__ . '/../../assert/mysql_running.php';
+
+Gaia\DB\Connection::load( array('test'=>function () {
+    return new Gaia\DB( new Gaia\DB\Driver\PDO('mysql:host=127.0.0.1;dbname=test;port=3306'));
+}
+) );
