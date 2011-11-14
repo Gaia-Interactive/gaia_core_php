a5f3c123d594ddb96baa64ab0ca017bc0d22e19e
John Loehrer <jloehrer@gaiaonline.com>
2011-11-14 12:18:21 -0800

    simplifying filter class

--- lib/gaia/filter.php
+++ lib/gaia/filter.php
@@ -4,65 +4,66 @@ namespace Gaia;
 class Filter
 {
 
-    public static function posint( $value, $default = NULL ){
-        return ctype_digit(strval($value)) ? $value : $default;
+    public static function posint( $value ){
+        return ctype_digit(strval($value)) ? $value : NULL;
     }
     
-    public static function int( $value, $default = NULL ){
+    public static function int( $value ){
         return (ctype_digit(strval($value)) ||
                       ($value[0] == '-' && ctype_digit(substr($value, 1)))
                       )
-                 ? $value : $default;
+                 ? $value : NULL;
     }
     
-    public static function alpha( $value, $default = NULL ){
-        return ctype_alpha(strval($value)) ? $value : $default;
+    public static function alpha( $value ){
+        return ctype_alpha(strval($value)) ? $value : NULL;
     }
     
-    public static function alphanum( $value, $default = NULL ){
-        return ctype_alnum(strval($value)) ? $value : $default;
+    public static function alphanum( $value ){
+        return ctype_alnum(strval($value)) ? $value : NULL;
     }
     
-    public static function numeric( $value, $default = NULL ){
-        return is_numeric($value) ? $value : $default;
+    public static function numeric( $value ){
+        return is_numeric($value) ? $value : NULL;
     }
     
-    public static function raw( $value, $default = NULL ){
-        return ( $value !== NULL ) ? $value : $default;
+    public static function raw( $value ){
+        return $value;
     }
     
     public static function bool( $value ){
             return (bool) $value ? TRUE : FALSE;
     }
     
-    public static function enum( $value, $pattern, $default = NULL){
-        return in_array($value, $pattern) ? $value : $default;
+    public static function enum( $value, $pattern ){
+        return in_array($value, $pattern) ? $value : NULL;
     }
     
-    public static function regex( $value, $pattern, $default = NULL ){
-        return preg_match($pattern, $value) ? $value : $default;
+    public static function regex( $value, $pattern ){
+        return preg_match($pattern, $value) ? $value : NULL;
     }
     
-    public static function utf8( $value, $default = NULL ){
-        $value = UTF8::to( $value );
-        if( $value === NULL ) $value = $default;
-        return $value;
+    public static function utf8( $value ){
+        return UTF8::to( $value );
     }
     
-    public static function safe( $value, $default = NULL ){
+    public static function safe( $value ){
         $unsafe = array('<', '>', '"', "'", '#', '&', '%', '{', '(');
         if( is_array( $value ) ){
-            foreach( $value as $k=>$v ) $value[ $k ] = str_replace($unsafe, '', strval($v));
+            foreach( $value as $k=>$v ){
+                $value[ $k ] = self::safe( $v );
+                if( $value[ $k ] === NULL ) unset( $value[ $k ] );
+            }
             return $value;
         } else {
             $value = str_replace($unsafe, '', strval($value));
         }
         
         // set to default value if there is nothing left after filtering
-        return $value !== '' ? $value : $default;
+        return $value !== '' ? $value : NULL;
     }
     
-    public static function against($value, $filter, $default = NULL ) {
+    public static function against($value, $filter ) {
         if( is_object( $value ) ) return $value;
         $options = NULL;
         if ( is_array($filter) ) {
@@ -70,16 +71,14 @@ class Filter
             switch($key ) {
             case 'regex':
             case 'enum':
-                return call_user_func( array( __CLASS__, $key ), $value, $filter[ $key ], $default );
+                return call_user_func( array( __CLASS__, $key ), $value, $filter[ $key ] );
             default:
-                $value = filter_var( $value, $key, $filter[ $key ] );
-                    return $value ? $value : $default;
+                return filter_var( $value, $key, $filter[ $key ] );
             }
         }
         if( ! method_exists( __CLASS__, $filter ) ){
-            $value = filter_var( $value, $filter );
-            return $value ? $value : $default;
+            return filter_var( $value, $filter );
         }
-        return call_user_func( array( __CLASS__, $filter ), $value, $default );
+        return call_user_func( array( __CLASS__, $filter ), $value );
     }
 }
