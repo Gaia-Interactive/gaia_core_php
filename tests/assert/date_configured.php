a266be6ed56e4676ae9938800589e81f9331202e
John Loehrer <jloehrer@gaiaonline.com>
2011-11-15 13:16:51 -0800

    more concise skip message about timezone

--- tests/assert/date_configured.php
+++ tests/assert/date_configured.php
@@ -1,5 +1,5 @@
 <?php
 use Gaia\Test\Tap;
 if( ! ini_get('date.timezone') ) {
-    Tap::plan('skip_all', "php ini date.timezone not set.See http://www.php.net/manual/en/function.date-default-timezone-get.php");
+    Tap::plan('skip_all', "php ini date.timezone not configured.");
 }
