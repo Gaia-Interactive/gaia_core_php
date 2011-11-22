664c7edcdea86321d122818482bbfd2b666f8198
John Loehrer <jloehrer@gaiaonline.com>
2011-11-22 11:24:20 -0800

    working version of views

--- lib/gaia/store/couchbase.php
+++ lib/gaia/store/couchbase.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @copyright 2003-present GAIA Interactive, Inc.
+ * EXPERIMENTAL.
+ * This API could change dramatically over the next few cycles of iteration.
+ * unstable, and for demonstration purposes only.
  */
 namespace Gaia\Store;
 use Gaia\Http;
@@ -10,31 +12,133 @@ use Gaia\Exception;
 
 class Couchbase extends Wrap {
     
-    protected $url = '';
+    protected $resturl = '';
     protected $s;
+    protected $design;
+    protected $http;
     
-    public function __construct(  $url = null,$core = null ){
+    // a simple wrapper that matches on the design name prefix.
+    const MAP_TPL = "function(doc){ if( doc._id.substr(0, %d) == '%s') { var inner = %s; inner(doc);}}";
+    
+    public function __construct($design,  $url = null, $core = null){
+        $design = trim($design, '/ ');
+        if( strlen( $design ) < 1 || ! preg_match('#^[a-z0-9_]+$#i', $design ) ) {
+            throw new Exception('invalid design', $design );
+        }
+        $design .= '/';
         if( $url === NULL ) $url = 'http://127.0.0.1:5984/default/';
         $this->s = new Json('');
+        $this->design = $design;
         if( ! $core instanceof Memcache ) $core = new Memcache( $core );
-        $core = new Serialize($core, $this->s);
+        $core = new Prefix( new Serialize($core, $this->s), $this->design);
         parent::__construct( $core );
-        $this->url = $url;
+        $this->resturl = $url;
     }
     
-    public function query( $design, $view, $params = NULL ){
+    public function getView( $view, $params = NULL ){
         $params = new Container( $params );
+        $show_metadata = $params->show_metadata;
+        unset( $params->show_metadata );
         if( ! $params->limit ) $params->limit = 10;
          if( ! $params->limit ) $params->skip = 0;
          if( ! $params->connection_timeout ) $params->connection_timeout = 60000;
-        $url = $this->url;
-        $http = new Http\Request( $url . '_design/' . $design . '/_view/' . $view . '/?' . http_build_query( $params->all()) );
+        $url = $this->resturl;
+        $http = $this->http = new Http\Request( $url . '_design/' . $this->design . '_view/' . $view . '/?' . http_build_query( $params->all()) );
         $http->serializer = $this->s;
         $response = $http->exec();
-        print $response;
         if( $response->http_code != '200' ) throw new Exception('query failed', $http );
         if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
+        $rows = array();
+        $len = strlen( $this->design );
+        foreach($response->body['rows'] as $data ){
+            $fields = array();
+            if( isset( $data['id'] ) ) {
+                $key = substr( $data['id'], $len );
+            } else {
+                $key = NULL;
+            }
+            if( is_array( $data['value'] ) ){
+                foreach( $data['value'] as $k => $v ){
+                    if( ! $show_metadata ){
+                        $firstchar = substr($k, 0, 1);
+                        if( $firstchar == '_' || $firstchar == '$' ) continue;
+                    } else {
+                        if( $k == '_id' ) $v = $key;
+                    }
+                    $fields[ $k ] = $v;
+                }
+            }
+            if( $key === NULL ) {
+                $rows[] = $fields;
+            } else {
+                $rows[ $key ] = $fields;
+            }
+        }
+        return  array('total_rows'=> $response->body['total_rows'], 'rows'=>$rows );
+    }
+    
+    public function createView($name, $map, $reduce ='' ){
+        $url = $this->resturl;
+        $http = $this->http = new Http\Request( $url . '_design/' . $this->design);
+        $http->serializer = $this->s;
+        $response = $http->exec();
+        $result = $response->body;
+        if( ! in_array( $response->http_code, array(200, 201, 404) )  ) throw new Exception('query failed', $http );
+        if( ! is_array( $result ) ) $result = array();
+        if( isset( $result['error'] ) ){
+            if( $result['error']  == 'not_found' ){
+                $result = array();
+            } else {
+                throw new Exception('query failed', $http );
+            }
+        }
+        if( ! isset ( $result['views'] ) ) $result['views'] = array();
+        if( $map === NULL ){
+            unset( $result['views'][$name] );
+        } else {
+            $len = strlen( $this->design );
+            if( $len > 0 ){
+                $map = sprintf( self::MAP_TPL, $len, $this->design, $map );
+            }
+            $result['views'][$name] = array('map'=>$map);
+            if( $reduce ) $result['views'][$name]['reduce'] = $reduce;
+        }
+        $http->post = $result;
+        $http->method = 'PUT';
+        $response = $http->exec();
+        if( ! in_array( $response->http_code, array(200, 201) )  ) throw new Exception('query failed', $http );
+        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
         return $response->body;
+    }
+    
+    
+     public function deleteAllViews(){
+        $url = $this->resturl;
+        $http = $this->http = new Http\Request( $url . '_design/' . $this->design);
+        $http->serializer = $this->s;
+        $response = $http->exec();
+        $result = $response->body;
+        if( $response->http_code == 404 ) return TRUE;
+        if( $response->http_code !== 200 ) throw new Exception('query failed', $http );        
+        $http->url = $http->url . '?rev=' . $result['_rev'];
+        $http->method = 'DELETE';
+        $response = $http->exec();
+        if( ! in_array( $response->http_code, array(200, 201) )  ) throw new Exception('query failed', $http );
+        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
+        return $response->body;
+    }
 
+    
+    
+    public function deleteView( $name ){
+        return $this->createView( $name, $map = NULL, $reduce = NULL );
+    }
+    
+    public function flush(){
+        
+    }
+    
+    public function http(){
+        return $this->http;
     }
 }
--- tests/store/couchbase.t
+++ tests/store/couchbase.t
@@ -8,12 +8,45 @@ include __DIR__ . '/../assert/memcache_installed.php';
 include __DIR__ . '/../assert/curl_installed.php';
 include __DIR__ . '/../assert/couchbase_running.php';
 
-Tap::plan(1);
+Tap::plan(10);
 
-$cache = new Store\Prefix(new Store\Couchbase(), md5( __FILE__ . '/' . microtime(TRUE) . '/' . php_uname()));
-$cache->addServer('127.0.0.1', '11211');
-$res = $cache->query('dev_v1', 'test', array('limit'=>5));
+$view = 'test' . time();
 
-Tap::ok( is_array( $res ), 'query returned an array');
+$cb = new Store\Couchbase('dev_v1' . time(), 'http://127.0.0.1:5984/default/', '127.0.0.1:11211');
+Tap::ok( $cb instanceof Store\Couchbase, 'instantiated couchbase');
 
-Tap::debug($res);
+$res = $cb->createView($view, 'function(doc){ emit(doc._id, doc);}', '');
+Tap::ok( $res['ok'], 'created a simple view');
+sleep(1);
+//Tap::debug( $res );
+
+$status = TRUE;
+$rows = array();
+for( $i = 0; $i < 3; $i++){
+    $res = $cb->set( $key = 'fun.' . microtime(TRUE), $row = array('foo'=>'bar', 'bazz'=>'quux', 'checksum'=>md5(microtime(TRUE))), 3600 );
+    $rows[ $key ] = $cb->get( $key );
+    if( ! $res ) $status = FALSE;
+}
+Tap::ok( $status, 'set all the data in without problems');
+sleep(1);
+$res = $cb->getView($view, array('limit'=>20, 'full_set'=>'true'));
+Tap::ok( is_array( $res ), 'got back a view of the data');
+Tap::is($res['rows'],$rows, 'all the rows results match what we put in');
+Tap::is( $res['total_rows'], $i, 'total_rows matches how many rows we inserted');
+
+$res = $cb->getView($view, array('limit'=>20, 'show_metadata'=>'1'));
+Tap::ok( is_array( $res ), 'got back a view with metadata');
+
+$ids = array();
+foreach( $res['rows'] as $row ){
+    $ids[] = $row['_id'];
+}
+Tap::cmp_ok( $ids, '===', array_keys( $rows ), 'internal ids match up');
+//Tap::debug($res );
+$res = $cb->deleteView($view);
+Tap::ok( $res['ok'], 'deleted the the view');
+$res = $cb->deleteAllViews();
+Tap::ok( $res['ok'], 'deleted all the views');
+
+
+//Tap::debug( $res );
