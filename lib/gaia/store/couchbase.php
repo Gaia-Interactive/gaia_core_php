40ea0637f83e8583e18c977cca39cb7dd4ba4364
John Loehrer <jloehrer@gaiaonline.com>
2011-11-23 11:11:14 -0800

    devs dont have to specify app if they dont want to

--- lib/gaia/store/couchbase.php
+++ lib/gaia/store/couchbase.php
@@ -2,7 +2,139 @@
 /**
  * EXPERIMENTAL.
  * This API could change dramatically over the next few cycles of iteration.
- * unstable, and for demonstration purposes only.
+ * unstable, for demonstration purposes only.
+ *
+ * Couchbase works like memcache for key/value access, but it writes the data into couchdb-like
+ * storage so you can access views of the data through the rest API. Instead of databases like couchdb
+ * has, couchbase has buckets. If you don't use the dedicated port for that bucket you need to use the 
+ * binary memcached protocol with SASL authentication to access one of the other buckets. Since most php 
+ * clients don't do this well, you need to set up a dedicated port for each bucket.
+ *
+ * In the development environment, that would mean a new bucket for each developer for each application.
+ * I found it was easier for everyone to share the same bucket, but just use key prefixes to avoid
+ * data collisions. If we apply the same key prefix idea to the views transparently, we can all share 
+ * the same bucket and still avoid data collisions.
+ *
+ * That means use of different buckets is a performance optimization, not a design constraint. I namespace
+ * the views in design document locations, and then transparently wrap the emit and map reduce calls 
+ * with a special javascript function to make the keys match what we put in without the namespace. 
+ *
+ * An example may help explain. I am building an app called zoo. during development, I name my app:
+ * 
+ *    $app = 'dev_jloehrer.zoo';
+ * 
+ * The prefix of dev_  let's couchbase know that the view can be applied to a subset of the data most 
+ * recently being manipulated but doesn't have to be applied across the cluster to all the data in the
+ * bucket. the jloehrer part is my username so that my keys don't collide with anyone else's keys.
+ *
+ * I instantiate my object:
+ *
+ *      $cb = new Couchbase( array(
+ *                      'app'       => 'dev_jloehrer-zoo',
+ *                      'rest'      => 'http://127.0.0.1:5984/default/',
+ *                      'socket'    => '127.0.0.1:11211',
+ *              ));
+ *
+ * The rest url provided allows us to communicate with the couchdb core of couchbase to get view
+ * information, while the socket is the uber-fast memcached protocol connection to couchbase for 
+ * key/value access.
+ *
+ * Now let's set a key into our application:
+ *
+ *      $cb->set('bear', array('type'=>'mammal', 'eats'=>'meat', legs'=>4 ) );
+ *
+ * Gaia\Store\Couchbase stores the data into couchbase with the key: 
+ *
+ *      dev_jloehrer-zoo/bear:
+ *
+ *  The value is automatically serialized json:
+ *  
+ *      telnet localhost 11211
+ *      Trying 127.0.0.1...
+ *      Connected to localhost.localdomain (127.0.0.1).
+ *      Escape character is '^]'.
+ *      get dev_jloehrer-zoo/bear
+ *      VALUE dev_jloehrer-zoo/bear 0 40
+ *      {"type":"mammal","eats":"meat","legs":4}
+ *      END
+ *
+ * This allows couchbase to do map reduce operations on the data in the views. For more on how views
+ * work, read up on how couchdb views work: 
+ *
+ *      http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views
+ *
+ *  couchbase works almost identically (with a few very subtle differences).
+ *
+ * Let's create our first view:
+ *
+ *      $cb->saveView('mammals', 'function(doc){ if( doc.type=="mammal" ){ emit(doc._id, doc); }}');
+ *
+ * this is a very simple javascript map function that we'll use to find all the mammals in our app.
+ * Views are just documents like any other object stored in couchbase.  We can take a look at it by 
+ * viewing the url:
+ *
+ *    $http = new \Gaia\Http\Request("http://127.0.0.1:5984/default/_design/dev_jloehrer-zoo/");
+ *    $result = json_decode( $http->exec()->body, TRUE);
+ *    echo $result['views']['mammals']['map'];
+
+ * 
+ * outputs:
+ *
+ *        function(doc){ 
+ *            if( doc._id.substr(0, 17) == 'dev_jloehrer-zoo/') { 
+ *                var d = eval(uneval(doc));
+ *                d._id = doc._id.substr(17);
+ *                var inner = function(doc){ if( doc.type=="mammal" ){ emit(doc._id, doc); }}; 
+ *                inner(d);  
+ *            }
+ *        }
+ *    
+ *
+ * Gaia\Store\Couchbase decorated our map function with some extra javascript. It makes sure that
+ * it only emits if the key has our application prefix. the internal doc._id function is rewritten 
+ * to not have the prefix. This makes our calls to the view work properly.
+ *
+ * Now we are ready to query our new view:
+ *
+ *    $result = $cb->view('mammals', array('full_set'=>'true'));
+ *    print_r( $result );
+ *
+ * This will output something similar to:
+ *
+ *     Array
+ *     (
+ *         [total_rows] => 1
+ *         [rows] => Array
+ *             (
+ *                 [0] => Array
+ *                     (
+ *                         [id] => bear
+ *                         [key] => bear
+ *                         [value] => Array
+ *                             (
+ *                                 [_id] => bear
+ *                                 [_rev] => 14-17c29c395ff470849cbec896b17df884
+ *                                 [$flags] => 0
+ *                                 [$expiration] => 0
+ *                                 [type] => mammal
+ *                                 [eats] => meat
+ *                                 [legs] => 4
+ *                             )
+ *     
+ *                     )
+ *     
+ *             )
+ *     
+ *     )
+ *
+ * If you don't specify an app, it doesn't do key prefixes for you, and all of the views are stuck in
+ * the default design document:
+ *
+ *    http://127.0.0.1:5984/default/_design/default/
+ *
+ * For more examples or details, see:
+ *    
+ *    tests/store/couchbase.t
  */
 namespace Gaia\Store;
 use Gaia\Http;
@@ -14,7 +146,7 @@ class Couchbase extends Wrap {
     
     protected $rest = '';
     protected $s;
-    protected $app;
+    protected $app = '';
     protected $http;
     
     // a simple wrapper that matches on the design name prefix.
@@ -28,85 +160,86 @@ class Couchbase extends Wrap {
         }
     }";
     
-    public function __construct(array $params ){
-        if( ! isset( $params['app'] ) ) {
-            trigger_error('must specify app to instantiate ' . __CLASS__, E_USER_ERROR);
-            exit;
-        }
     
-        $app = trim($params['app'], '/ ');
-        if( strlen( $app ) < 1 || ! preg_match('#^[a-z0-9_]+$#i', $app ) ) {
-            trigger_error('invalid app name for ' . __CLASS__, E_USER_ERROR);
-            exit;
-        }
-        
-        $this->app = $app .'/';
-
-        if( ! isset( $params['rest'] ) ) {
-            trigger_error('must specify rest to instantiate ' . __CLASS__.'. example: http://127.0.0.1:5984/default/', E_USER_ERROR);
-            exit;
-        }
-        
+    
+    /**
+    * Instantiate the couchbase object. pass in a set of named params.
+    * Example:
+    *      $cb = new Couchbase( array(
+    *                      'app'       => 'myapp',
+    *                      'rest'      => 'http://127.0.0.1:5984/default/',
+    *                      'socket'    => '127.0.0.1:11211',
+    *              ));
+    */
+    public function __construct(array $params ){
+        if( ! isset( $params['app'] ) ) $params['app'] = '';
+        $app = isset( $params['app'] ) ? trim($params['app'], '/ ') : '';
+        if( strlen( $app ) > 0 ) $this->app = $app .'/';
+        if( ! isset( $params['rest'] ) )  $params['rest'] = '';
         $this->rest = $params['rest'];
         $this->s = new Json('');
-        $core = isset( $params['core'] ) ? $params['core'] : NULL;
+        $core = isset( $params['socket'] ) ? $params['socket'] : NULL;
         if( ! $core instanceof Memcache ) $core = new Memcache( $core );
         $core = new Prefix( new Serialize($core, $this->s), $this->app);
         parent::__construct( $core );
     }
     
+    /**
+    * get back a the view results.
+    * Example:
+    *    $params = array(
+    *       'startkey'=>'bear',
+    *       'endkey'=>'zebra',
+    *       'connection_timeout'=> 60000,
+    *       'limit'=>10,
+    *       'skip'=>0,
+    *       'full_set'=>'true',
+    *    );
+    *    $result = $cb->view('mammals' $params);
+    *
+    */
     public function view( $view, $params = NULL ){
         $params = new Container( $params );
-        if( ! $params->limit ) $params->limit = 10;
-         if( ! $params->limit ) $params->skip = 0;
-         if( $params->startkey ) $params->startkey = json_encode($params->startkey );
-         if( $params->endkey ) $params->startkey = json_encode($params->endkey );
-         if( $params->key ) $params->key = json_encode($params->key );
-         if( ! $params->connection_timeout ) $params->connection_timeout = 60000;
-        $http = $this->request( '_design/' . $this->app . '_view/' . $view . '/?' . http_build_query( $params->all()) );
-        $response = $http->exec();
-        if( $response->http_code != '200' ) throw new Exception('query failed', $http );
-        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
-        $result = $response->body;
-        $rows = array();
-        $len = strlen( $this->app );
-        $rows = $response->body['rows'];
-        foreach($rows as $i => $row ){
-            if( isset( $row['id'] ) ) {
-                $rows[$i]['id'] = substr( $row['id'], $len );
+        foreach( $params as $k => $v ) {
+            if( ! is_scalar( $v ) || preg_match('#key#i', $k) ){
+                $params->$k = json_encode( $v );
             }
         }
-        $result['rows'] = $rows;
+        $len = strlen( $this->app );
+        $app = ( $len > 0 ) ? $this->app : 'default/';
+        $http = $this->request( '_design/' . $app . '_view/' . $view . '/?' . http_build_query( $params->all()) );
+        $response = $this->validateResponse( $http->exec(), array(200) );
+        $result = $response->body;
+        if( $len < 1 ) return $result;
+        foreach($result['rows'] as & $row ){
+            if( isset( $row['id'] ) ) {
+                $row['id'] = substr( $row['id'], $len );
+            }
+        }
+        
         //print_r( $result );
         return $result;
     }
     
-    public function preview( $view, $params = NULL ){
-        $result = $this->view( $view, $params );
-        $rows = array();
-        foreach( $result['rows'] as $row ){
-            $key = isset( $row['id'] ) ? $row['id'] : NULL;
-            if( is_array( $row['value'] ) ){
-                foreach( $row['value'] as $k => $v ){
-                    $firstchar = substr($k, 0, 1);
-                    if( $firstchar == '_' || $firstchar == '$' ) unset( $row['value'][$k]);
-                }
-            }
-            if( $key !== NULL ){
-                $rows[$key] = $row['value'];
-            } else {
-                $rows[] = $row['value'];
-            }
-        }
-        $result['rows'] = $rows;
-        return $result;
-    }
-    
+    /*
+    * create or overwrite a view.
+    * Example:
+    *
+    *    $res = $cb->saveView('amount', 'function(doc){ emit(doc._id, doc.amount);}', '_sum');
+    *
+    * This uses the built-in _sum reduce function that can give you the sum of all the results.
+    * or just specify a map function:
+    *
+    *    $res = $cb->saveView('full', 'function(doc){ emit(doc._id, {foo: doc.foo});}');
+    *
+    * returns an ok status along with a document rev id.
+    */
     public function saveView($name, $map, $reduce ='' ){
-        $http = $this->request( '_design/' . $this->app);
-        $response = $http->exec();
+        $len = strlen( $this->app );
+        $app = ( $len > 0 ) ? $this->app : 'default/';
+        $http = $this->request( '_design/' . $app);
+        $response = $this->validateResponse( $http->exec(), array(200, 201, 404) );
         $result = $response->body;
-        if( ! in_array( $response->http_code, array(200, 201, 404) )  ) throw new Exception('query failed', $http );
         if( ! is_array( $result ) ) $result = array();
         if( isset( $result['error'] ) ){
             if( $result['error']  == 'not_found' ){
@@ -119,53 +252,74 @@ class Couchbase extends Wrap {
         if( $map === NULL ){
             unset( $result['views'][$name] );
         } else {
-            $len = strlen( $this->app );
-            $map = sprintf( self::MAP_TPL, $len, $this->app, $len, $map );
-            
+            if( $len ) $map = sprintf( self::MAP_TPL, $len, $this->app, $len, $map );
             $result['views'][$name] = array('map'=>$map);
             if( $reduce ) $result['views'][$name]['reduce'] = $reduce;
         }
         $http->post = $result;
         $http->method = 'PUT';
-        $response = $http->exec();
-        if( ! in_array( $response->http_code, array(200, 201) )  ) throw new Exception('query failed', $http );
-        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
+        $response = $this->validateResponse( $http->exec(), array(200, 201) );
         return $response->body;
     }
     
-    
-     public function deleteAllViews(){
-        $http = $this->request( '_design/' . $this->app);
-        $response = $http->exec();
+    /**
+    * deletes all of the views created in a given app namespace.
+    */
+    public function deleteAllViews(){
+        $len = strlen( $this->app );
+        $app = ( $len > 0 ) ? $this->app : 'default/';
+        $http = $this->request( '_design/' . $app);
+        $response = $this->validateResponse( $http->exec(), array(404, 200) );
         $result = $response->body;
         if( $response->http_code == 404 ) return TRUE;
-        if( $response->http_code !== 200 ) throw new Exception('query failed', $http );        
         $http->url = $http->url . '?rev=' . $result['_rev'];
         $http->method = 'DELETE';
-        $response = $http->exec();
-        if( ! in_array( $response->http_code, array(200, 201) )  ) throw new Exception('query failed', $http );
-        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $http );
+        $response = $this->validateResponse( $http->exec(), array(200, 201) );
         return $response->body;
     }
 
-    
-    
+    /**
+    * deletes a specific view in a given app namespace.
+    * Example:
+    *
+    *   $cb->deleteView('amount');
+    *
+    * throws an exception on error, returns an ok status and rev id on success.
+    */
     public function deleteView( $name ){
         return $this->saveView( $name, $map = NULL, $reduce = NULL );
     }
-    
+        
+    /**
+    * haven't quite figured out how to do this yet. Need the equivalent of _all_docs, but narrowed 
+    * down to just the docs in my app.
+    */
     public function flush(){
         
     }
     
+    /*
+    * temporary debug method, do not use.
+    */
     public function http(){
         return $this->http;
     }
     
-    
+    /*
+    * temporary debug method, do not use.
+    */
     public function request($path){
         $http = $this->http = new Http\Request( $this->rest . $path );
         $http->serializer = $this->s;
         return $http;
+    }
+    
+    /**
+    * handle a response.
+    */
+    protected function validateResponse( \Gaia\Container $response, array $allowed_codes ){
+        if( ! in_array( $response->http_code, $allowed_codes )  ) throw new Exception('query failed', $response );
+        if( ! is_array( $response->body ) ) throw new Exception('invalid response', $response );
+        return $response;
     }
 }
--- tests/store/couchbase.t
+++ tests/store/couchbase.t
@@ -13,15 +13,28 @@ include __DIR__ . '/../assert/memcache_running.php';
 function waitforRebuild($iterations = 10 ){
     print "# waiting for view rebuild ";
     for( $i = 0; $i < $iterations; $i++){
-        usleep(100000);
+        usleep(500000);
         print ".";
     }
     print "\n";
 }
 
-Tap::plan(19);
+$extract_data = function( $result ){
+    $rows = array();
+    foreach( $result['rows'] as $row ){
+        $key = isset( $row['id'] ) ? $row['id'] : NULL;
+        if( $key !== NULL ){
+            $rows[$key] = $row['value'];
+        } else {
+            $rows[] = $row['value'];
+        }
+    }
+    return $rows;
+};
+
+Tap::plan(22);
 try {    
-    $cb = new Store\Couchbase(array( 'app'=> 'dev_v' . time(), 'rest'=>'http://127.0.0.1:5984/default/', 'core'=>'127.0.0.1:11211'));
+    $cb = new Store\Couchbase(array( 'app'=> 'app-v.' . time(), 'rest'=>'http://127.0.0.1:5984/default/', 'socket'=>'127.0.0.1:11211'));
     Tap::ok( $cb instanceof Store\Couchbase, 'instantiated couchbase');
     
     //Tap::debug( $res );
@@ -38,16 +51,16 @@ try {
     }
     Tap::ok( $status, 'set all the data in without problems');
     
-    $res = $cb->saveView('full', 'function(doc){ emit(doc._id, doc);}', '');
+    $res = $cb->saveView('full', 'function(doc){ emit(doc._id, {foo: doc.foo, bazz: doc.bazz, amount: doc.amount, ts: doc.ts});}', '');
     Tap::ok( $res['ok'], 'created a full view');
     
     $res = $cb->saveView('nokey', 'function(doc){ emit(doc._id, null);}', '');
     Tap::ok( $res['ok'], 'created a view with no data result');
 
-    $res = $cb->saveView('2keys', 'function(doc){ emit([doc._id, doc.ts], doc);}', '');
+    $res = $cb->saveView('2keys', 'function(doc){ emit([doc._id, doc.ts], {foo: doc.foo, bazz: doc.bazz, amount: doc.amount, ts: doc.ts});}', '');
     Tap::ok( $res['ok'], 'created a view emitting two keys');
     
-    $res = $cb->saveView('2keys', 'function(doc){ emit([doc._id, doc.ts], doc);}', '');
+    $res = $cb->saveView('2keys', 'function(doc){ emit([doc._id, doc.ts], {foo: doc.foo, bazz: doc.bazz, amount: doc.amount, ts: doc.ts});}', '');
     Tap::ok( $res['ok'], 'created a view emitting two keys');
     
     
@@ -55,15 +68,17 @@ try {
     Tap::ok( $res['ok'], 'created a view summing the amount');
     
     waitforRebuild(10);
-    $res = $cb->preview('full', array('limit'=>20, 'full_set'=>'true'));
+    $res = $cb->view('full', array('limit'=>20, 'full_set'=>'true'));
+    $result_set = $extract_data( $res );
     Tap::ok( is_array( $res ), 'got back a view of the data');
-    Tap::is($res['rows'],$rows, 'all the rows results match what we put in');
+    Tap::is($result_set,$rows, 'all the rows results match what we put in');
     Tap::is( $res['total_rows'], $i, 'total_rows matches how many rows we inserted');
     
-    $res = $cb->preview('full', array('limit'=>20, 'show_metadata'=>'1'));
+    $res = $cb->view('full', array('limit'=>20, 'show_metadata'=>'1'));
+    $result_set = $extract_data( $res );
     Tap::ok( is_array( $res ), 'got back a view with metadata');
     
-    $ids = array_keys($res['rows']);
+    $ids = array_keys($result_set);
     
     Tap::cmp_ok( $ids, '===', array_keys( $rows ), 'internal ids match up');
     //Tap::debug($res );
@@ -71,22 +86,35 @@ try {
     Tap::ok( $res['ok'], 'deleted the the view');
     
     
-    $res = $cb->preview('nokey', array('limit'=>20, 'full_set'=>'true'));
+    $res = $cb->view('nokey', array('limit'=>20, 'full_set'=>'true'));
+    $result_set = $extract_data( $res );
     Tap::ok( is_array( $res ), 'got back a view of the keys');
-    Tap::is($res['rows'], array_fill_keys(array_keys($rows), array()), 'got back the results with empty arrays as placeholders');
+    Tap::is($result_set, array_fill_keys(array_keys($rows), array()), 'got back the results with empty arrays as placeholders');
     
     
     $keys = array_keys( $rows );
-    $res = $cb->preview('2keys', array('limit'=>20, 'full_set'=>'true', 'startkey'=>array($keys[0]), 'endkey'=>array($keys[1])));
+    $res = $cb->view('2keys', array('limit'=>20, 'full_set'=>'true', 'startkey'=>array($keys[0]), 'endkey'=>array($keys[1])));
+    $result_set = $extract_data( $res );
     Tap::ok( is_array( $res ), 'got back a view of the data for 2 keys');
     //Tap::debug( $res );
     
-    $res = $cb->preview('amount');
+    $res = $cb->view('amount');
+    $result_set = $extract_data( $res );
     Tap::ok( is_array( $res ), 'got back a view of the data for summing an amount');
-    Tap::is( $res['rows'][0], $total, 'total matches up');
+    Tap::is( $result_set[0], $total, 'total matches up');
 
     $res = $cb->deleteAllViews();
     Tap::ok( $res['ok'], 'deleted all the views');
+    
+    
+    $cb = new Store\Couchbase(array( 'rest'=>'http://127.0.0.1:5984/default/', 'socket'=>'127.0.0.1:11211'));
+    Tap::ok( $cb instanceof Store\Couchbase, 'instantiated couchbase without the app prefix');
+    
+    $res = $cb->saveView('test', 'function(doc){ emit(doc._id, doc); }');
+    Tap::ok(is_array( $res ), 'created a view in the default design');
+    $res = $cb->deleteView('test');
+    Tap::ok( $res['ok'], 'deleted the the view');
+   
     
     
     //Tap::debug( $res );
