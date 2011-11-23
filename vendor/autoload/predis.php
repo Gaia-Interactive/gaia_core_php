1b2a81d3895520843827fb15f61f9169a6b50120
John Loehrer <jloehrer@gaiaonline.com>
2011-11-23 14:56:05 -0800

    dont need predis autoload hook anymore. it is in the main autoload hook

--- vendor/autoload/predis.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// predis autoloader.
-spl_autoload_register(function($class) {
-    if( substr( strtolower($class), 0, 7) != 'predis\\' ) return;
-    @include  __DIR__.'/../predis/lib/'.strtr($class, '\\', '/').'.php';    
-});
-
