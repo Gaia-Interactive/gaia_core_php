078ada92025a2d131c0439267a51071cad686155
John Loehrer <jloehrer@gaiaonline.com>
2011-11-23 14:38:10 -0800

    little bit more complex autoload function to handle all of the vendor libs too but it is more efficient this way. don't have to worry about merge conflicts since it is in the framework branch, so the glob search for vendor autoload includes isn't necessary anymore.

--- autoload.php
+++ autoload.php
@@ -1,14 +1,39 @@
 <?php
 
-// gaia's core auto-loader
+// gaia's core auto-loader and all the vendor libs.
 spl_autoload_register(function($class) {
+    // force classname to lowercase ... make sure we are standardizing it.
     $class = strtolower($class);
-    if( substr( $class, 0, 5) != 'gaia\\' ) return;
-    include  __DIR__.'/lib/'.strtr($class, '\\', '/').'.php';    
+    
+    // load facebook related classes.
+    if( $class == 'facebook' ) 
+        return @include  __DIR__ . '/vendor/facebook/php-sdk/src/facebook.php';
+    if( $class == 'basefacebook' ) 
+        return @include  __DIR__ . '/vendor/facebook/php-sdk/src/base_facebook.php';
+    
+    // load yaml vendor classes
+    if( $class == 'sfyaml' ) 
+        return @include  __DIR__ . '/vendor/yaml/lib/sfYaml.php';
+    
+    // check to see if the base pheanstalk object is loaded and include it if not.
+    // at the same time set up the Pheanstalk_ClassLoader for all the sub classes.
+    // don't want to have to do pheanstalk autoload hook until we need to since it is 
+    // a little expensive to do on every page load.
+    if( $class == 'pheanstalk') {
+        $class_dir = __DIR__ . '/vendor/pheanstalk/classes';
+        include $class_dir . '/Pheanstalk/ClassLoader.php';
+        Pheanstalk_ClassLoader::register($class_dir);
+        include $class_dir . '/Pheanstalk.php';
+    }
+    
+    // load the gaia namespaced classes.
+    if( substr( $class, 0, 5) == 'gaia\\' ) 
+        return include  __DIR__.'/lib/'.strtr($class, '\\', '/').'.php';   
+        
+    // load predis namespaced classes.
+    if( substr($class, 0, 7) == 'predis\\' ) 
+        return @include  __DIR__.'/vendor/predis/lib/'.strtr($class, '\\', '/').'.php';
+    
+    // all done.
 });
-
-foreach( glob( __DIR__ . '/vendor/autoload/*.php') as $file ){
-    if( substr(basename( $file ), 0, 1) == '.' ) continue;
-    require $file;
-}
 
