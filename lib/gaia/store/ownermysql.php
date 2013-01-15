d28fd1914946647186445e4d357b5b1a366b29a5
John Loehrer <jloehrer@gaiaonline.com>
2013-01-15 11:15:04 -0800

    allow closures to be passed into the gaia\store\{db} objects

--- lib/gaia/store/ownermysql.php
+++ lib/gaia/store/ownermysql.php
@@ -47,16 +47,7 @@ class OwnerMySQL implements Iface {
         $owner = strval( $owner );
         if( ! ctype_digit( $owner ) ) throw new Exception('invalid owner', $owner );
         $this->owner = $owner;
-        $this->db = function() use ( $db ){
-            static $object;
-            if( isset( $object ) ) return $object;
-            if( is_scalar( $db ) ) $db = DB\Connection::instance( $db );
-            if( ! $db instanceof DB\Iface ) throw new Exception('invalid db object');
-            if( ! $db->isA('mysql') ) throw new Exception('db object not mysql');
-            if( ! $db->isa('gaia\db\extendediface') ) throw new Exception('invalid db object');
-            if( ! $db->isA('Gaia\DB\Except') ) $db = new DB\Except( $db );
-            return $object = $db;
-        };
+        $this->db = $db;
         $this->table = $table;
         $this->s = ( $s ) ? $s : new \Gaia\Serialize\PHP;
     }
@@ -237,8 +228,18 @@ class OwnerMySQL implements Iface {
     }
     
     protected function db(){
-        $closure = $this->db;
-        $db = $closure();
+        if( $this->db instanceof \Closure ){
+            $mapper = $this->db;
+            $db = $mapper( $table );
+        } elseif( is_scalar( $this->db ) ){
+            $db = DB\Connection::instance( $this->db );
+        } else {
+            $db = $this->db;
+        }
+        if( ! $db instanceof DB\Iface ) throw new Exception('invalid db');
+        if( ! $db->isa('mysql') ) throw new Exception('invalid db');
+        if( ! $db->isa('gaia\db\extendediface') ) throw new Exception('invalid db');
+        if( ! $db->isa('Gaia\DB\Except') ) $db = new DB\Except( $db );
         if( DB\Transaction::inProgress() ) DB\Transaction::add( $db );
         return $db;
     }
