14910566846443229ad9a8c059446b3f006c5a56
John Loehrer <jloehrer@gaiaonline.com>
2013-06-07 10:25:40 -0700

    first quick port of identifier class into gaia core php

---
+++ lib/gaia/identifier/iface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Gaia\Identifier;
+
+interface Iface {   
+    public function byId( $request );
+    public function byName( $request  );
+    public function delete( $id, $name );
+    public function store( $id, $name, $strict = FALSE );
+    public function batch( \Closure $closure, array $options = NULL );
+
+}
---
+++ tests/identifier/mysql.t
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php
+
+include __DIR__ . '/mysql.setup.php';
+include __DIR__ .'/basic_test_suite.php';
